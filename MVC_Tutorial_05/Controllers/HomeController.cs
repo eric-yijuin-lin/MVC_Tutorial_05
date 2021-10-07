efc7067ab768e33bd70f8daeeeaef04e5d7c972f
Eric Lin <eric.yijuin.lin@gmail.com>
2021-10-07 12:24:51 +0800

    true initial commit

---
+++ MVC_Tutorial_05/Controllers/HomeController.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVC_Tutorial_05.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Tutorial_05.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult IssueCookies()
+        {
+            HttpContext.Session.SetString("TextKey", "Key123");
+            HttpContext.Session.SetInt32("IntKey", 12345);
+
+            var cookieOption = new CookieOptions()
+            {
+                Expires = new DateTimeOffset(DateTime.Now.AddSeconds(30))
+            };
+
+            HttpContext.Response.Cookies.Append(
+                "SimpleCookie", 
+                "cookie value in plain text",
+                cookieOption
+            );
+
+            var preference = new UserPreference()
+            {
+                Theme = "dark",
+                FontFamily = "Arial",
+                FontSize = "12"
+            };
+
+            HttpContext.Response.Cookies.Append(
+                "ComplexCookie",
+                JsonConvert.SerializeObject(preference),
+                cookieOption
+            );
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult CookiesTest()
+        {
+            var textKey = HttpContext.Session.GetString("TextKey");
+            var intKey = HttpContext.Session.GetInt32("IntKey");
+            var cookie = HttpContext.Request.Cookies["SimpleCookie"];
+            var cookie2 = JsonConvert.DeserializeObject<UserPreference>(
+                HttpContext.Request.Cookies["ComplexCookie"]
+            );
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult ModelValidationTest()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public DemoModelValidationResult ModelValidationTest(DemoModel model)
+        {
+            var result = new DemoModelValidationResult();
+            if (ModelState.IsValid)
+            {
+                result.IsValid = true;
+            }
+            else
+            {
+                result.Errors = new List<ValidationError>();
+                foreach (var key in ModelState.Keys)
+                {
+                    if (ModelState[key].Errors.Count > 0)
+                    {
+                        result.Errors.Add(new ValidationError() 
+                        { 
+                            FieldName = key,
+                            Message = ModelState[key].Errors[0].ErrorMessage
+                        });
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(DemoModel model)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        public void HeartBeat()
+        {
+            Console.WriteLine("beep");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVC_Tutorial_05/Startup.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Tutorial_05
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // ----- add these codes to use session
+            services.AddDistributedMemoryCache();
+            services.AddSession(option =>
+            {
+                option.IdleTimeout = TimeSpan.FromSeconds(5);
+                option.Cookie.Name = "MvcTutoralSession";
+                option.Cookie.HttpOnly = true;
+                option.Cookie.IsEssential = true; // CheckConsentNeeded 會防止送出 non-essential 的
+                // cookie, 設為 true 就代表不管，通通給我送
+            });
+            // -----
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            // ----- add this to use session, NOTE: before UseEndpoints() and after UseRouting()
+            app.UseSession();
+            // -----
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
